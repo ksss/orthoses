931e87a33431ad05eda67bf05123ee9d7ec8c25c
ksss <co000ri@gmail.com>
2022-06-01 23:02:30 +0900

    Support recursive mixin

--- lib/orthoses/avoid_recursive_ancestor_error.rb
+++ lib/orthoses/avoid_recursive_ancestor_error.rb
@@ -7,14 +7,21 @@ module Orthoses
     def call
       @loader.call.tap do |store|
         object_mixins = {}
-        store["Object"].to_decl.members.each do |member|
-          case member
-          when RBS::AST::Members::Mixin
-            object_mixins[member.name.relative!.to_s] = true
-          end
-        end
+        set_object_mixins_recursive(store, "Object", object_mixins)
+
         object_mixins.each_key do |object_mixin|
           store[object_mixin].header = "module #{object_mixin} : BasicObject"
+        end
+      end
+    end
+
+    def set_object_mixins_recursive(store, name, object_mixins)
+      store[name].to_decl.members.each do |member|
+        case member
+        when RBS::AST::Members::Mixin
+          member_name = member.name.relative!.to_s
+          object_mixins[member_name] = true
+          set_object_mixins_recursive(store, member_name, object_mixins)
         end
       end
     end
--- lib/orthoses/avoid_recursive_ancestor_error_test.rb
+++ lib/orthoses/avoid_recursive_ancestor_error_test.rb
@@ -2,7 +2,12 @@ module AvoidRecursiveAncestorErrorTest
   def test_avoid_recursive_ancestor_error(t)
     store = Orthoses::AvoidRecursiveAncestorError.new(->(){
       Orthoses::Utils.new_store.tap do |store|
-        store["Imod"]
+        store["Nested"].header = "module Nested"
+        store["Imod"].header = "module Imod"
+        store["Emod"].header = "module Emod"
+        store["Pmod"].header = "module Pmod"
+        store["Nested"]
+        store["Imod"] << "include Nested"
         store["Emod"]
         store["Pmod"]
         store["Object"] << "include Imod"
@@ -11,7 +16,7 @@ module AvoidRecursiveAncestorErrorTest
       end
     }).call
 
-    %w[Imod Emod Pmod].each do |mod|
+    %w[Nested Imod Emod Pmod].each do |mod|
       unless store[mod].to_rbs.include?(" : BasicObject")
         t.error("expect #{mod} with self_type `BasicObject` but got #{store[mod].header}")
       end
