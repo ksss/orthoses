f0cf828a740e8e74defa5e275039007f151e6a42
ksss <co000ri@gmail.com>
2022-06-06 10:17:37 +0900

    Use TracePoint instead of hook methods.
    - Increased accuracy
    - Avoid monkey patch

--- lib/orthoses/mixin.rb
+++ lib/orthoses/mixin.rb
@@ -2,49 +2,69 @@
 
 module Orthoses
   class Mixin
+    module Hook
+      def include(*modules)
+        super
+      end
+      def extend(*modules)
+        super
+      end
+      def prepend(*modules)
+        super
+      end
+    end
+
     def initialize(loader, if: nil)
       @loader = loader
       @if = binding.local_variable_get(:if)
     end
 
     def call
-      modules = Hash.new { |h, k| h[k] = [] }
-      ::Module.module_eval do
-        define_method(:included) do |mod|
-          modules[mod] << [:include, self]
-        end
+      ::Module.prepend(Hook)
 
-        define_method(:extended) do |mod|
-          modules[mod] << [:extend, self]
-        end
+      include = CallTracer.new
+      extend = CallTracer.new
+      prepend = CallTracer.new
 
-        define_method(:prepended) do |mod|
-          modules[mod] << [:prepend, self]
+      store = include.trace(::Module.method(:include)) do
+        extend.trace(::Module.method(:extend)) do
+          prepend.trace(::Module.method(:prepend)) do
+            @loader.call
+          end
         end
       end
 
-      store = @loader.call
+      collect_definitions(store, include, :include)
+      collect_definitions(store, extend, :extend)
+      collect_definitions(store, prepend, :prepend)
 
-      modules.each do |base_mod, how_mods|
+      store
+    end
+
+    private
+
+    def collect_definitions(store, call_tracer, how)
+      call_tracer.result.each do |method, argument|
+        base_mod = method.receiver
         next unless base_mod.kind_of?(Module)
+
         base_mod_name = Utils.module_name(base_mod)
         next unless base_mod_name
 
-        lines = how_mods.filter.map do |how, mod|
+        content = store[base_mod_name]
+        argument[:modules].each do |mod|
           mod_name = Utils.module_name(mod)
           next unless mod_name
+
           known_type_params = Utils.known_type_params(mod)
           next unless known_type_params.nil? || known_type_params.empty?
+
           next unless @if.nil? || @if.call(base_mod, how, mod)
 
           store[mod_name]
-
-          "#{how} #{mod_name}"
+          content << "#{how} #{mod_name}"
         end
-        store[base_mod_name].concat(lines)
       end
-
-      store
     end
   end
 end
