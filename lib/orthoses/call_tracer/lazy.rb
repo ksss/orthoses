3db798c220768d2f8326fde9e1273399fb4ac6aa
ksss <co000ri@gmail.com>
2022-08-06 15:28:22 +0900

    Fix bug

--- lib/orthoses/call_tracer/lazy.rb
+++ lib/orthoses/call_tracer/lazy.rb
@@ -2,20 +2,20 @@
 
 module Orthoses
   class CallTracer
-    class Lazy < LazyTracePoint
+    class Lazy
       include Capturable
 
       attr_reader :captures
 
       def initialize
         @captures = []
-        super(:call) do |tp|
+        @lazy_trace_point = LazyTracePoint.new(:call) do |tp|
           @captures << build_capture(tp)
         end
       end
 
       def trace(name, &block)
-        enable(target: name, &block)
+        @lazy_trace_point.enable(target: name, &block)
       end
     end
   end
