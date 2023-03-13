3a00daab8ff2621b07aed7295bae9f8804924536
ksss <co000ri@gmail.com>
2023-03-13 22:17:59 +0900

    Fix config loader
    RBS::Collection::Config.lockfile_of was deleded

--- lib/orthoses/utils.rb
+++ lib/orthoses/utils.rb
@@ -50,28 +50,6 @@ module Orthoses
       RBS::Namespace.parse(name).to_type_name
     end
 
-    class << self
-      # Generated scripts are not always located in the root.
-      # By default, it follows the upper level directory from the current directory
-      # to find the rbs_collection.yaml file and set it to `rbs_collection_pathname`.
-      # It can be reconfigured if necessary.
-      attr_accessor :rbs_collection_pathname
-    end
-    # set default
-    self.rbs_collection_pathname = ->() {
-      begin
-        start = here = Dir.pwd
-        until ok = RBS::Collection::Config::PATH.exist?
-          Dir.chdir("..")
-          return nil if Dir.pwd == here
-          here = Dir.pwd
-        end
-        Pathname(here) + RBS::Collection::Config::PATH
-      ensure
-        Dir.chdir(start)
-      end
-    }.call
-
     def self.rbs_environment(library: nil, collection: true, cache: true)
       @env_cache ||= {}
       if cache && hit = @env_cache[[library, collection]]
@@ -80,9 +58,15 @@ module Orthoses
 
       loader = RBS::EnvironmentLoader.new
 
-      if collection && rbs_collection_pathname
-        config = RBS::Collection::Config.lockfile_of(rbs_collection_pathname) or raise
-        loader.add_collection(config)
+      if collection
+        config_path = RBS::Collection::Config.find_config_path || RBS::Collection::Config::PATH || raise("needs rbs_collection.yaml")
+        lock_path = RBS::Collection::Config.to_lockfile_path(config_path)
+        raise "needs rbs_collection.yaml file" unless lock_path.file?
+        lock = RBS::Collection::Config::Lockfile.from_lockfile(
+          lockfile_path: lock_path,
+          data: YAML.load_file(lock_path.to_s)
+        )
+        loader.add_collection(lock)
       end
 
       case library
