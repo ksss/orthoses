a41591deed60fd4e3daa72fac18c13ba185ed006
ksss <co000ri@gmail.com>
2023-07-12 00:30:41 +0900

    Should check included interface method

--- lib/orthoses/content/duplication_checker.rb
+++ lib/orthoses/content/duplication_checker.rb
@@ -4,71 +4,86 @@ module Orthoses
     class DuplicationChecker
       def initialize(decl, env: nil)
         @decl = decl
-        @env = env || Utils.rbs_environment
+        @known_env = env || Utils.rbs_environment
+        @builder = RBS::DefinitionBuilder.new(env: @known_env)
+        @uniq_map = {}
       end
 
       def update_decl
         return unless @decl.respond_to?(:members)
 
-        uniq_map = uniq_members
-        drop_set_method_definition(uniq_map)
-        drop_known_method_definition(uniq_map)
+        uniq_members
+        drop_set_method_definition
+        drop_known_method_definition
+        drop_known_const_definition
 
-        @decl.members.replace(uniq_map.values)
+        @decl.members.replace(@uniq_map.values)
+
+        @uniq_map.clear
       end
 
       private
 
       def uniq_members
-        {}.tap do |uniq_map|
-          @decl.members.each do |member|
-            if member.instance_of?(RBS::AST::Members::MethodDefinition) && member.overloading?
-              # avoid to duplicate and keep order
-              uniq_map[Object.new] = member
-            else
-              key = member_key(member)
-              drop_member = uniq_map[key]
-              uniq_map[key] = member
-              if drop_member
-                Orthoses.logger.info("#{@decl.name} \"#{member.location.source}\" was droped since duplication")
-              end
+        @decl.members.each do |member|
+          if member.instance_of?(RBS::AST::Members::MethodDefinition) && member.overloading?
+            # avoid to duplicate and keep order
+            @uniq_map[Object.new] = member
+          else
+            key = member_key(member)
+            drop_member = @uniq_map[key]
+            @uniq_map[key] = member
+            if drop_member
+              Orthoses.logger.info("#{@decl.name} \"#{member.location.source}\" was droped since duplication")
             end
           end
         end
       end
 
-      def drop_set_method_definition(uniq_map)
-        attr_accessors = uniq_map.values.grep(RBS::AST::Members::AttrAccessor)
+      def drop_set_method_definition
+        attr_accessors = @uniq_map.values.grep(RBS::AST::Members::AttrAccessor)
         attr_accessors.each do |member|
-          uniq_map.delete([RBS::AST::Members::MethodDefinition, :"#{member.name}=", member.kind])
+          @uniq_map.delete([RBS::AST::Members::MethodDefinition, :"#{member.name}=", member.kind])
         end
       end
 
-      def drop_known_method_definition(uniq_map)
+      def drop_known_method_definition
         decl_name = @decl.name.absolute!
-        if m_entry = @env.class_decls[decl_name]
+        if m_entry = @known_env.class_decls[decl_name]
           m_entry.decls.each do |d|
-            d.decl.members.each do |member|
-              case member
-              when RBS::AST::Members::LocationOnly
-                # ignore
-              when RBS::AST::Members::AttrAccessor
-                uniq_map.delete([RBS::AST::Members::MethodDefinition, member.name, member.kind])
-                uniq_map.delete([RBS::AST::Members::MethodDefinition, :"#{member.name}=", member.kind])
-              else
-                uniq_map.delete(member_key(member))
-              end
+            drop_known_method_definition_recur(d)
+          end
+        end
+      end
+
+      def drop_known_method_definition_recur(d)
+        d.decl.members.each do |member|
+          case member
+          when RBS::AST::Members::LocationOnly
+            # ignore
+          when RBS::AST::Members::AttrAccessor
+            @uniq_map.delete([RBS::AST::Members::MethodDefinition, member.name, member.kind])
+            @uniq_map.delete([RBS::AST::Members::MethodDefinition, :"#{member.name}=", member.kind])
+          else
+            if member.kind_of?(RBS::AST::Members::Include) && member.name.interface?
+              # If interface is included, it is shared in the same namespace.
+              drop_known_method_definition_recur(@known_env.interface_decls[member.name])
+            else
+              @uniq_map.delete(member_key(member))
             end
           end
         end
+      end
 
-        constants_in_uniq_map = uniq_map.select do |key, value|
+      def drop_known_const_definition
+        decl_name = @decl.name.absolute!
+        constants_in_uniq_map = @uniq_map.select do |key, value|
           value.kind_of?(RBS::AST::Declarations::Constant)
         end
         constants_in_uniq_map.each do |key, value|
           type_name = decl_name + value.name
-          if @env.constant_decls[type_name]
-            uniq_map.delete(key)
+          if @known_env.constant_decls[type_name]
+            @uniq_map.delete(key)
           end
         end
       end
