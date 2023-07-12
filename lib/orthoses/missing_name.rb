7a0e5a9ae23ba253e0b00017de6815261fe5af86
ksss <co000ri@gmail.com>
2023-07-12 12:02:18 +0900

    Refactoring

--- lib/orthoses/missing_name.rb
+++ lib/orthoses/missing_name.rb
@@ -8,57 +8,74 @@ module Orthoses
 
     def call
       @loader.call.tap do |store|
-        missing_class(store)
-        missing_module(store)
+        MissingClass.new(store).call
+        MissingModule.new(store).call
       end
     end
 
     private
 
-    def missing_module(store)
-      missings = []
-      store.keys.each do |key_name|
-        missings.concat missing_names(store, key_name)
+    class MissingClass
+      def initialize(store)
+        @store = store
       end
-      missings.uniq!
-      missings.each do |missing|
-        store[missing].header = "module #{missing}"
-      end
-    end
 
-    def missing_class(store)
-      recur = ->(content) {
+      def call
+        @store.values.each do |content|
+          recur(content)
+        end
+      end
+
+      private
+
+      def recur(content)
         content.auto_header
         if content.header && content.header.include?("<")
           _, superclass = content.header.split(/\s*<\s*/, 2)
           superclass.sub!(/\[.*/, "")
           new_name = TypeName(superclass).relative!.to_s
-          recur.call(store[new_name])
+          recur(@store[new_name])
         end
-      }
-      store.values.each do |content|
-        recur.call(content)
       end
     end
 
-    def missing_names(store, key_name)
-      ret = []
-
-      type_name = TypeName(key_name).relative!
-      if !Utils.rbs_defined_class?(type_name.to_s) && !store.has_key?(type_name.to_s)
-        ret << type_name.to_s
+    class MissingModule
+      def initialize(store)
+        @store = store
       end
 
-      path = type_name.namespace.path
-      path.each_with_index do |sym, index|
-        name = path[0, index + 1].join("::")
-        next if name.empty?
-        if !Utils.rbs_defined_class?(name) && !store.has_key?(name)
-          ret << name
+      def call
+        missings = []
+        @store.keys.each do |key_name|
+          missings.concat split_name(key_name)
+        end
+        missings.uniq!
+        missings.each do |missing|
+          @store[missing].header = "module #{missing}"
         end
       end
 
-      ret
+      private
+
+      def split_name(key_name)
+        ret = []
+
+        type_name = TypeName(key_name).relative!
+        if !Utils.rbs_defined_class?(type_name.to_s) && !@store.has_key?(type_name.to_s)
+          ret << type_name.to_s
+        end
+
+        path = type_name.namespace.path
+        path.each_with_index do |sym, index|
+          name = path[0, index + 1].join("::")
+          next if name.empty?
+          if !Utils.rbs_defined_class?(name) && !@store.has_key?(name)
+            ret << name
+          end
+        end
+
+        ret
+      end
     end
   end
 end
