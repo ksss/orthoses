2bf0247c14a70c045030e8abd82e031c65c79f8f
ksss <co000ri@gmail.com>
2023-08-04 23:20:12 +0900

    Fix integration_test

--- integration_test/active_support_test.rb
+++ integration_test/active_support_test.rb
@@ -2,11 +2,14 @@ module ActiveSupportTest
   def test_active_support(t)
     Orthoses::Builder.new do
       use Orthoses::CreateFileByName,
-        to: 'integration_test/tmp'
+        to: 'integration_test/tmp',
+        depth: 2,
+        rmtree: true
       use Orthoses::Filter do |name, content|
         name.start_with?("ActiveSupport") || name.start_with?("Integer")
       end
       use Orthoses::Constant, strict: false
+      use Orthoses::Attribute
       use Orthoses::Mixin
       use Orthoses::ObjectSpaceAll
       use Orthoses::Autoload
