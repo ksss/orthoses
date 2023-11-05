87ad3f11abebf279681e1e1bf9276bc4ba945742
ksss <co000ri@gmail.com>
2023-11-05 16:43:48 +0900

    Support unnamed parameters

--- lib/orthoses/call_tracer/capturable.rb
+++ lib/orthoses/call_tracer/capturable.rb
@@ -3,6 +3,17 @@
 module Orthoses
   class CallTracer
     module Capturable
+      module ExtractRestParameters
+        def __extract_rest_parameters__(*rest, **kw_rest, &block)
+          {
+            :* => rest,
+            :** => kw_rest,
+            :& => block,
+            :"..." => [rest, kw_rest, block]
+          }
+        end
+      end
+
       def build_capture(tp)
         Capture.new(
           method: build_method(tp),
@@ -17,11 +28,12 @@ module Orthoses
       end
 
       def build_argument(tp)
+        has_unnamed_parameters = false
         tp.parameters.each_with_object({}) do |op_name, hash|
           name = op_name[1]
           case name
           when :*, :**, :&, nil
-            # skip
+            has_unnamed_parameters = true
           else
             hash[name] = tp.binding.local_variable_get(name).then do |var|
               case var
@@ -34,6 +46,12 @@ module Orthoses
               warn("#{var.class}#dup failed with #{err.class}:#{err.message}")
               var
             end
+          end
+        end.tap do |hash|
+          if has_unnamed_parameters
+            tp.binding.eval("extend ::Orthoses::CallTracer::Capturable::ExtractRestParameters")
+            unnamed_parameters = tp.binding.eval("__extract_rest_parameters__(...)")
+            hash.merge!(unnamed_parameters)
           end
         end
       end
--- lib/orthoses/call_tracer_test.rb
+++ lib/orthoses/call_tracer_test.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+begin
+  require 'test_helper'
+rescue LoadError
+end
+
 module CallTracerTest
   class Foo
     def method
@@ -18,6 +23,45 @@ module CallTracerTest
     c.captures.each do |capture|
       unless capture.instance_of?(Orthoses::CallTracer::Capture)
         t.error("break instance #{capture}")
+      end
+    end
+  end
+
+  class UnnamedParameters
+    def receive(a, b = nil, c:)
+    end
+
+    def captured(a, ...)
+      receive(a, ...)
+    end
+  end
+
+  def test_unnamed_parameters(t)
+    c = Orthoses::CallTracer.new
+    c.trace(UnnamedParameters.instance_method(:captured)) do
+      UnnamedParameters.new.captured(:a, 'b', c: 'ccc') {}
+    end
+    c.captures.each do |capture|
+      unless capture.argument[:a] == :a
+        t.error("could not capture named argument")
+      end
+      unless capture.argument[:*] == ['b']
+        t.error("could not capture `*` argument")
+      end
+      unless capture.argument[:**] == {c: 'ccc'}
+        t.error("could not capture `*` argument")
+      end
+      unless capture.argument[:&].kind_of?(Proc)
+        t.error("could not capture `&` argument")
+      end
+      unless capture.argument[:"..."][0] == ['b']
+        t.error("could not capture `...` argument")
+      end
+      unless capture.argument[:"..."][1] == {c: 'ccc'}
+        t.error("could not capture `...` argument")
+      end
+      unless capture.argument[:"..."][2].kind_of?(Proc)
+        t.error("could not capture `...` argument")
       end
     end
   end
