9045c272dd93c23e08d3a4a7a34692dc9ed5cd5f
ksss <co000ri@gmail.com>
2024-10-29 11:00:18 +0900

    Fix sort error from rbs v3.6

---
+++ lib/orthoses/sort_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+begin
+  require 'test_helper'
+rescue LoadError
+end
+
+module SortTest
+  def test_sort(t)
+    store = Orthoses::Builder.new do
+      use Orthoses::Sort
+      use Orthoses::Tap do |store|
+        store["Foo"].header = "class Foo"
+        store["Foo"] << "def foo: () -> void"
+        store["Foo"] << "def baz: () -> void"
+        store["Foo"] << "def bar: () -> void"
+      end
+      run -> () {}
+    end.call
+
+    actual = store.map { |n, c| c.to_rbs }.join("\n")
+    expect = <<~RBS
+      class Foo
+        def bar: () -> void
+        def baz: () -> void
+        def foo: () -> void
+      end
+    RBS
+    unless expect == actual
+      t.error("expect=\n```rbs\n#{expect}```\n, but got \n```rbs\n#{actual}```\n")
+    end
+  end
+end
