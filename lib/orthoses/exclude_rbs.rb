a28db66559d9ff406fb2457066df7950c4e9ac92
ksss <co000ri@gmail.com>
2025-07-16 22:06:27 +0900

    Rebuild environment after add signature

--- lib/orthoses/exclude_rbs.rb
+++ lib/orthoses/exclude_rbs.rb
@@ -25,12 +25,20 @@ module Orthoses
         raise "bug"
       end
 
+      rebuild_environment
+
       @loader.call
     end
 
     def add_signature_to_known_env(rbs)
       buffer, directives, decls = ::RBS::Parser.parse_signature(rbs)
       Utils.rbs_environment.add_signature(buffer: buffer, directives: directives, decls: decls)
+    end
+
+    def rebuild_environment
+      old_env = Utils.rbs_environment
+      new_env = old_env.resolve_type_names
+      Utils.module_eval { @env_cache[[nil, true]] = new_env }
     end
   end
 end
